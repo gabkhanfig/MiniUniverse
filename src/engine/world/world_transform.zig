04cf175bd9847c0c976d9c409664cc3102ccad29
gabkhanfig <gabkhanfig@gmail.com>
2024-03-14 01:20:26 -0400

    BlockIndex

--- src/engine/world/world_transform.zig
+++ src/engine/world/world_transform.zig
@@ -1,13 +1,36 @@
+const CHUNK_LENGTH = 256;
+const CHUNK_SIZE: comptime_int = CHUNK_LENGTH * CHUNK_LENGTH;
+
+/// Position of a block within a chunk.
+/// x has a factor of 1.
+/// y has a factor of CHUNK_LENGTH
+pub const BlockIndex = extern struct {
+    const Self = @This();
+
+    x: u8,
+    y: u8,
+
+    pub fn index(self: Self) u16 {
+        const x: u16 = @intCast(self.x);
+        const y: u16 = @intCast(self.y);
+        return x | @shlExact(y, 8);
+    }
+};
+
+/// Position of a chunk in the world.
+/// Used with the `SpatialChunkHashGrid`.
 pub const ChunkPosition = struct {
+    const Self = @This();
+
     x: i32,
     y: i32,
     layer: i8,
 
-    pub fn eql(self: *const @This(), other: @This()) bool {
+    pub fn eql(self: *const Self, other: Self) bool {
         return self.x == other.x and self.y == other.y and self.layer == other.layer;
     }
 
-    pub fn hash(self: @This()) usize {
+    pub fn hash(self: Self) usize {
         var h: usize = 0;
 
         h |= @intCast(self.x);
